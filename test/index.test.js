595c26948711370056465fb6b2076d968f5c2c53
Jason Stangroome <jason@section.io>
2016-08-19 22:59:02 +1000

    Test processEvent

--- test/index.test.js
+++ test/index.test.js
@@ -6,6 +6,8 @@ var index = require('../index');
 
 describe('index', function () {
 
+    this.timeout(5000);
+
     describe('handler', function () {
 
         var anEvent = {
@@ -65,6 +67,33 @@ describe('index', function () {
                     return Promise.resolve();
                 });
             });
+
+        });
+
+    });
+
+    describe('processEvent', function () {
+
+        it('should', function (done) {
+
+            var event = {
+                "body": "text=go%20good%20news%20everyone"
+            };
+            var context = {
+                succeed: function (message) {
+                    expect(message.response_type).to.equal('in_channel');
+                    expect(message.attachments).ok;
+                    expect(message.attachments.length).to.equal(1);
+                    expect(message.attachments[0].title).ok;
+                    expect(message.attachments[0].title_link).ok;
+                    expect(message.attachments[0].image_url).ok;
+                    done();
+                },
+                fail: function (err) {
+                    done(err);
+                },
+            };
+            index.processEvent(event, context);
 
         });
 
