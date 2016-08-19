745c4524be826984b1f3e09f369a8860e2902b2a
Jason Stangroome <jason@section.io>
2016-08-19 22:28:37 +1000

    Test nothing found

--- test/index.test.js
+++ test/index.test.js
@@ -33,20 +33,37 @@ describe('index', function () {
 
     describe('getMemeImageUrl', function () {
 
-        var resultPromise = index.getMemeImageUrl('good news everyone')
+        describe('find "good news everyone"', function () {
 
-        it('should have an imageUrl', function () {
+            it('should have an imageUrl', function () {
 
-            return resultPromise.then(function (result) {
-                expect(result.imageUrl).match(/^https:\/\/morbotron\.com\/meme\/S\d{2}E\d{2}\/\d+\.jpg\?b64lines=.+/);
+                var result = index.getMemeImageUrl('good news everyone');
+                return result.then(function (result) {
+                    expect(result.imageUrl).match(/^https:\/\/morbotron\.com\/meme\/S\d{2}E\d{2}\/\d+\.jpg\?b64lines=.+/);
+                });
+
+            });
+
+            it('should have an captionUrl', function () {
+
+                var result = index.getMemeImageUrl('good news everyone');
+                return result.then(function (result) {
+                    expect(result.captionUrl).match(/^https:\/\/morbotron\.com\/caption\/S\d{2}E\d{2}\/\d+$/);
+                });
+
             });
 
         });
 
-        it('should have an captionUrl', function () {
+        describe('find nothing', function () {
 
-            return resultPromise.then(function (result) {
-                expect(result.captionUrl).match(/^https:\/\/morbotron\.com\/caption\/S\d{2}E\d{2}\/\d+$/);
+            it('should reject promise', function () {
+                var result = index.getMemeImageUrl('kjfkhduerureujskjshlkjekurruruzqxcq');
+                return result.then(function () {
+                    return Promise.reject();
+                }, function (err) {
+                    return Promise.resolve();
+                });
             });
 
         });
